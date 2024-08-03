906b85a24ec8d34152c307899501f005ca14ef4f
Krehy <samuel.krehy@gmail.com>
2024-08-04 00:17:32 +0200

    Dokončení první verze CMS

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,10 @@
-from wagtail_modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail_modeladmin.options import ModelAdmin, ModelAdminGroup, modeladmin_register
 from .models import Developer, Publisher, Genre, Platform, BlogPost, Game, Review
 
 class DeveloperAdmin(ModelAdmin):
     model = Developer
     menu_label = 'Developers'
-    menu_icon = 'user'  # Specifikujte název ikony
+    menu_icon = 'user'
     list_display = ('name',)
     search_fields = ('name',)
 
@@ -13,7 +13,7 @@ modeladmin_register(DeveloperAdmin)
 class PublisherAdmin(ModelAdmin):
     model = Publisher
     menu_label = 'Publishers'
-    menu_icon = 'group'  # Specifikujte název ikony
+    menu_icon = 'group'
     list_display = ('name',)
     search_fields = ('name',)
 
@@ -22,7 +22,7 @@ modeladmin_register(PublisherAdmin)
 class GenreAdmin(ModelAdmin):
     model = Genre
     menu_label = 'Genres'
-    menu_icon = 'tag'  # Specifikujte název ikony
+    menu_icon = 'tag'
     list_display = ('name',)
     search_fields = ('name',)
 
@@ -31,7 +31,7 @@ modeladmin_register(GenreAdmin)
 class PlatformAdmin(ModelAdmin):
     model = Platform
     menu_label = 'Platforms'
-    menu_icon = 'desktop'  # Specifikujte název ikony
+    menu_icon = 'desktop'
     list_display = ('name',)
     search_fields = ('name',)
 
@@ -39,27 +39,28 @@ modeladmin_register(PlatformAdmin)
 
 class BlogPostAdmin(ModelAdmin):
     model = BlogPost
-    menu_label = 'Články'  # Změňte název na 'Články'
-    menu_icon = 'doc-full'  # Specifikujte název ikony
+    menu_label = 'Články'
+    menu_icon = 'doc-full'
     list_display = ('title', 'intro', 'read_count')
     search_fields = ('title', 'intro')
-
-modeladmin_register(BlogPostAdmin)
 
 class GameAdmin(ModelAdmin):
     model = Game
-    menu_label = 'Hry'  # Změňte název na 'Hry'
-    menu_icon = 'media'  # Použijte ikonu dostupnou ve Wagtailu
+    menu_label = 'Hry'
+    menu_icon = 'media'
     list_display = ('title', 'description', 'developer', 'publisher')
     search_fields = ('title', 'description')
 
-modeladmin_register(GameAdmin)
-
 class ReviewAdmin(ModelAdmin):
     model = Review
-    menu_label = 'Recenze'  # Změňte název na 'Recenze'
-    menu_icon = 'edit'  # Specifikujte název ikony
+    menu_label = 'Recenze'
+    menu_icon = 'edit'
     list_display = ('title', 'intro', 'read_count')
     search_fields = ('title', 'intro')
 
-modeladmin_register(ReviewAdmin)
+class ContentAdminGroup(ModelAdminGroup):
+    menu_label = 'Obsah webu'
+    menu_icon = 'folder-open-inverse'  # Ikona pro hlavní položku menu
+    items = (BlogPostAdmin, GameAdmin, ReviewAdmin)
+
+modeladmin_register(ContentAdminGroup)
